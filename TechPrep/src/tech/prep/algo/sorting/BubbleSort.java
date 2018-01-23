7364c7b959f4752a661e1df621429959d168f3bf
amberpandey <amberpandey@192.168.1.8>
2018-01-23 02:59:36 -0800

    Bubblesort

--- TechPrep/src/tech/prep/algo/sorting/BubbleSort.java
+++ TechPrep/src/tech/prep/algo/sorting/BubbleSort.java
@@ -2,4 +2,27 @@ package tech.prep.algo.sorting;
 
 public class BubbleSort {
 
+	public static int[] sort(int[] arr){
+		for(int j=0;j<arr.length;j++){
+			boolean swapped=false;
+		for(int i=0; i<arr.length-1;i++){
+			if(arr[i]>arr[i+1]){
+				int temp= arr[i+1];
+				arr[i+1]=arr[i];
+				arr[i]=temp;
+				swapped=true;
+			}
+		}
+		if(!swapped)return arr;
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args){
+		int[] arr= {2,5,9,3,1,7,6};
+		
+		for(int i:sort(arr)){
+			System.out.println(i);	
+		}
+	}
 }
---
+++ TechPrep/src/tech/prep/ds/LinkedLists/DNode.java
@@ -0,0 +1,13 @@
+package tech.prep.ds.LinkedLists;
+
+public class DNode {
+	public DNode nextNode;
+	public DNode prevNode;
+	public int value;
+	public DNode(int value){
+		nextNode=null;
+		prevNode=null;
+		this.value=value;
+	}
+
+}
